138809ce9eaf1f1c2874da29b1899347bed2c481
mcboeing <56201456+mcboeing@users.noreply.github.com>
2025-09-28 15:10:24 -0700

    added new page link for 'events'

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -158,7 +158,8 @@ export const approveEvent = catchAsync(async (req, res, next) => {
       email: event.createdBy.email,
       subject: `Your Event Submission: "${event.title}" has been Approved!`,
       html: `<p>Great news! Your event, "${event.title}", has been approved and is now live on TourMyGarage.</p>
-      <p>Check it out!</p><p>Question?  Ask our pit crew: <a href="mailto:support@tourmygarage.com">support@tourmygarage.com</a></p>`,
+      <p>Check it out!   <a href="https://www.tourmygarage.com/events" target="_blank" rel="noopener noreferrer">Events</a>
+      </p><p>Question?  Ask our pit crew: <a href="mailto:support@tourmygarage.com">support@tourmygarage.com</a></p>`,
     });
   } catch (err) {
     console.error('Failed to send approval email:', err);
